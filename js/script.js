64f9f268ee27ee0a9738ee49ba59ce4f724a87be
christiancamilleri <camilleri.christian@outlook.com>
2023-03-09 15:03:01 +0100

    feat: milensone 0

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Dato un array di oggetti letterali con:
+//  - url dell’immagine
+//  - titolo
+//  - descrizione
+
+
+// milenstone 0
+// crea container
+// inserisci immagine
+
+// bersaglio container-img
+let containerImgEl = document.getElementById("container-img");
+
+containerImgEl.style.backgroundImage = "url(../img/01.webp)";
+containerImgEl.style.backgroundSize = "cover";
+containerImgEl.style.backgroundRepeat = "no-repeat"
